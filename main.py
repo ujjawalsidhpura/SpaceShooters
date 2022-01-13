d6468185e5b8ade2792e1090c5971a36fc30bed6
ujjawal sidhpura <ujjawalsidhpura@gmail.com>
2022-01-13 12:03:57 -0500

    level and lives disp[lay

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+from ctypes.wintypes import RGB
 import pygame
 import os
 import random
 import time
+pygame.font.init()
 
 WIDTH, HEIGHT = 900, 900
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -31,10 +33,20 @@ BG = pygame.transform.scale(pygame.image.load(
 def main():
     run = True
     FPS = 60
+    level = 1
+    lives = 3
+    main_font = pygame.font.SysFont('arial', 50)
+
     clock = pygame.time.Clock()
 
     def redraw_window():
         WIN.blit(BG, (0, 0))
+        # draw text
+        lives_label = main_font.render(f'Lives : {lives}', 1, (255, 255, 255))
+        level_label = main_font.render(f'Level : {level}', 1, (255, 255, 255))
+
+        WIN.blit(lives_label, (10, 10))
+        WIN.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
         pygame.display.update()
 
     while run:
